7dc330a92c7c12a9818b9a2147a7e6b37d90fc90
Geoffrey M Gunter <geoffrey.m.gunter@gmail.com>
2024-07-15 12:44:30 -0700

    Add missing module

---
+++ src/whirlwind/_unwrap.py
@@ -0,0 +1,35 @@
+import numpy as np
+from numpy.typing import ArrayLike
+
+from ._cost import compute_carballo_costs
+from ._lib import residue as get_residues
+from ._lib import integrate_unwrapped_gradients
+from .graph import RectangularGridGraph
+from .network import Network, primal_dual
+
+__all__ = [
+    "unwrap",
+]
+
+
+def unwrap(
+    igram: ArrayLike,
+    corr: ArrayLike,
+    nlooks: float,
+    *,
+    mask: ArrayLike | None = None,
+) -> np.ndarray:
+    """ """
+    phase = np.angle(igram)
+
+    residue = get_residues(phase)
+
+    surplus = residue.flatten()
+    cost = compute_carballo_costs(igram, corr, nlooks, mask)
+
+    graph = RectangularGridGraph(*residue.shape)
+    network = Network(graph, surplus, cost, capacity=1)
+
+    primal_dual(network, maxiter=8)
+
+    return integrate_unwrapped_gradients(phase, network._impl)
